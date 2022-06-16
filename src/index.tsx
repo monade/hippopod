e6d43edffd499cf28b09ef938e5b9302a7e536c7
Daniel Fornarini <daniel@monade.io>
2022-06-16 16:39:49 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -39,6 +39,4 @@ reportWebVitals();
 //   'https://www.omnycontent.com/d/playlist/60311b15-274a-4e3f-8ba9-ac3000834f37/0c2cf638-f8c9-4d9e-8cc3-ae7f01104abc/c1aea8ba-bda1-487d-ac8d-ae7f01104ad8/podcast.rss',
 //   'https://feeds.megaphone.fm/storiedibrand'
 // ]
-// for (const feed of feeds) {
-//   getPodcast(feed).then(podcast => console.log(podcast));
-// }
+
