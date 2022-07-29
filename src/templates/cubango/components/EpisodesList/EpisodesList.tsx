2c0815213774249645284022f7c83bb64094853e
Federico Mainetti Gambera <federicomainettigambera@Federicos-MBP.home>
2022-07-29 16:27:46 +0200

    fix episodes images

--- src/templates/cubango/components/EpisodesList/EpisodesList.tsx
+++ src/templates/cubango/components/EpisodesList/EpisodesList.tsx
@@ -1,45 +1,56 @@
-import React, {useEffect, useState} from 'react';
-import './EpisodesList.scss';
-import {Episode} from "../../../../models/episode";
+import React, { useEffect, useState } from "react";
+import "./EpisodesList.scss";
+import { Episode } from "../../../../models/episode";
 import EpisodesListItem from "../EpisodesListItem/EpisodesListItem";
 
 interface Props {
   episodes: Episode[];
-  imageUrl?: string;
   onPlayEpisode: (episode: Episode) => void;
   onQueueEpisode: (episode: Episode) => void;
 }
 
-const EpisodesList: React.FC<Props> = ({ episodes, imageUrl, onPlayEpisode, onQueueEpisode }) => {
+const EpisodesList: React.FC<Props> = ({
+  episodes,
+  onPlayEpisode,
+  onQueueEpisode,
+}) => {
   const [shownEpisodes, setShownEpisodes] = useState<Episode[]>([]);
 
   useEffect(() => {
     setShownEpisodes(sliceEpisodes(4));
   }, []);
 
-  const sliceEpisodes = (episodesAmount: number) => episodes.slice(0, Math.min(episodesAmount, episodes.length));
-  const showMore = () => setShownEpisodes(sliceEpisodes(shownEpisodes.length + 4));
+  const sliceEpisodes = (episodesAmount: number) =>
+    episodes.slice(0, Math.min(episodesAmount, episodes.length));
+  const showMore = () =>
+    setShownEpisodes(sliceEpisodes(shownEpisodes.length + 4));
 
-  const renderEpisodes = () => shownEpisodes.map((episode, index) =>
-    <EpisodesListItem
-      key={`listEpisode${index}`}
-      episode={episode}
-      imageUrl={imageUrl}
-      onPlayEpisode={onPlayEpisode}
-      onQueueEpisode={onQueueEpisode}
-    />
-  );
+  const renderEpisodes = () =>
+    shownEpisodes.map((episode, index) => (
+      <EpisodesListItem
+        key={`listEpisode${index}`}
+        episode={episode}
+        imageUrl={episode.imageUrl}
+        onPlayEpisode={onPlayEpisode}
+        onQueueEpisode={onQueueEpisode}
+      />
+    ));
 
-  const renderShowMoreButton = () => episodes.length !== shownEpisodes.length ?
-    <button className='episodes-list__toggle' onClick={() => showMore()}>Load more</button> :
-    '';
+  const renderShowMoreButton = () =>
+    episodes.length !== shownEpisodes.length ? (
+      <button className="episodes-list__toggle" onClick={() => showMore()}>
+        Load more
+      </button>
+    ) : (
+      ""
+    );
 
   return (
-    <div className='episodes-list'>
+    <div className="episodes-list">
       {renderEpisodes()}
       {renderShowMoreButton()}
     </div>
   );
-}
+};
 
 export default EpisodesList;
