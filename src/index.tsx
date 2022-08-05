5ef14f4aca253625b9b71c3177f52e0bc9dd47d2
Daniel Fornarini <daniel@monade.io>
2022-08-05 15:27:03 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom/client";
 import { ARGUMENTS } from './data/arguments';
 import "./index.scss";
 import { PlayerProvider } from "./store/playerContext";
-import Bani from "./templates/bani/bani";
-import Cubango from "./templates/cubango/cubango";
-import Zambezi from "./templates/zambezi/zambezi";
+
+const Theme = React.lazy(() => import(`./templates/${ARGUMENTS.theme}/${ARGUMENTS.theme}`));
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,22 +13,8 @@ const root = ReactDOM.createRoot(
 const localConfig = localStorage.getItem('config') as any;
 const config = localConfig ? JSON.parse(localConfig) : ARGUMENTS;
 
-switch(config.theme) {
-    case 'bani':
-        root.render(
-            <Bani />
-        );
-        break;
-    case 'cubango':
-        root.render(
-          <PlayerProvider>
-            <Cubango color={(config as any).color} themeMode={(config as any).themeMode} links={(config as any).links || {}} socials={(config as any).socials || {}} />
-          </PlayerProvider>
-        );
-        break;
-    case 'zambezi':
-        root.render(
-            <Zambezi />
-        )
-        break;
-}
+root.render(
+  <PlayerProvider>
+    <Theme color={(config as any).color} themeMode={(config as any).themeMode} links={(config as any).links || {}} socials={(config as any).socials || {}} />
+  </PlayerProvider>
+)
